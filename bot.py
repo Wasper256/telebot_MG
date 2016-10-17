3fa361cc7db2f243db48e4df53c87ba707feaaea
wasper256 <andremorhun@gmail.com>
2016-10-17 18:21:29 +0300

    main upgrade

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
 import telebot
+from const import token, text
+
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=['new_chat_member'])
+def send_welcome(message):
+    """Main Decorator, check and send message in case if new member in chat."""
+    user_id = message.new_chat_member.id
+    bot.send_message(user_id, text)
+    print("New chat member received the message")
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
--- const.py
+++ const.py
@@ -1,2 +1,9 @@
-token = "<Your bot token here>"
-message = "<Here your message>"
+# -*- coding: utf-8 -*-
+token = "206749085:AAHB00ih1Ul_kPZ7Bl7v8lrOIc05idpWvXk"
+message = """
+
+<here your message><Тут ваше повідомлення><Сдесь ваше сообщение>
+Йоу Настьона, якщо ти бачиш це то напиши мені.=)
+
+"""
+text = message.decode('utf8')
