19a9d7d398371cf382f57118f6d5366dbe2528fb
Wasper <Wasper256@users.noreply.github.com>
2016-10-18 19:13:43 +0300

    Update const.py

--- const.py
+++ const.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-token = "206749085:AAHB00ih1Ul_kPZ7Bl7v8lrOIc05idpWvXk"
+token = ""
 message = """
-
 <here your message><Тут ваше повідомлення><Сдесь ваше сообщение>
-Йоу Настьона, якщо ти бачиш це то напиши мені.=)
 
 """
 text = message.decode('utf8')
