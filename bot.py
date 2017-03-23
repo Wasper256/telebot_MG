51392b8e99c19566a1e6490e3cca69721186d9b0
Wasper <Wasper256@users.noreply.github.com>
2017-03-23 16:28:15 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 import telebot
-from const import token, text
+import logging
+from const import token, text, text2
 
-
+logging.basicConfig(filename='telebot.log', level=logging.INFO, format='%(asctime)s %(message)s')
 bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(content_types=['new_chat_member'])
-def send_welcome(message):
-    """Main Decorator, check and send message in case if new member in chat."""
+def new_chat_member(message):
+    """Main decorator, check and send message in case if new member in chat."""
     user_id = message.new_chat_member.id
-    bot.send_message(user_id, text)
-    print("New chat member received the message")
+    responce = bot.send_message(user_id, text)
+    logging.warning("New chat member({}) received the message, /n {}".format(user_id, responce))
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    responce = bot.reply_to(message, text2)
+    logging.warning('{}, added bot to contacts/n{}'.format(message.from_user, responce))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0)
--- const.py
+++ const.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-token = ""
+token = "<<here your token>>"
 message = """
-<here your message><Тут ваше повідомлення><Сдесь ваше сообщение>
-
+Чек-лист “Как создать свой инструмент коммуникации в Telegram и повысить продажи?”
+https://docs.google.com/document/d/1mbUYbzNHEeDEAxNLQXqnWVXdARAjqgMYeEWzTf8cKRU/edit?usp=shar
+"""
+message2 = """
+Здравствуй! Чтобя я выслал вам чек-лист “Как создать свой инструмент коммуникации в Telegram и повысить продажи?” необходимо присоединиться к груповому чату Marketing Gamers (https://telegram.me/joinchat/Ed0FuUAD_YYZ3PCsNeAceQ)
 """
 text = message.decode('utf8')
+text2 = message2.decode('utf8')
